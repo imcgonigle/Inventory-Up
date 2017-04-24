1f2a8d2d8e62ae556c365266c9ddd2d9a8339df4
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-24 16:09:15 -0600

    add clarity-design system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
+
+import { ClarityModule } from 'clarity-angular';
 
 const appRoutes: Routes = [
   {path: 'landing', component: LandingComponent },
@@ -21,7 +25,9 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    ClarityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
