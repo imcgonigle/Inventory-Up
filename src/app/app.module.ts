1dfa2573cf0cac4f2ab18c62ee368cd7dafcda58
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-24 15:39:50 -0600

    add landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
