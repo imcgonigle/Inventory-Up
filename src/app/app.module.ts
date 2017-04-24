25b042453b31186ac7806b15b8b64e5cd90cd3b1
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-24 15:47:15 -0600

    add RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
+
+const appRoutes: Routes = [
+  {path: 'landing', component: LandingComponent },
+  {path: '', redirectTo: '/landing', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +20,8 @@ import { LandingComponent } from './pages/landing/landing.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
